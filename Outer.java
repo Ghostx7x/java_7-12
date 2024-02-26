3bdf3a485395403be74c6cd28476a2ce6313b4da
Ghostx7x <112379929+Ghostx7x@users.noreply.github.com>
2024-02-26 20:07:37 +0530

    Add files via upload

---
+++ Outer.java
@@ -0,0 +1,15 @@
+class Outer 
+{
+    public void display()
+    {
+        System.out.println("Outer class display method");
+    }
+    class Inner
+    {
+        public void display()
+        {
+            System.out.println("Inner class display method");
+        }
+    }
+}
+
