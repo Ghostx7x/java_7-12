cf3f187e6b17a20ffe08bbd6b216808348b0871e
Ghostx7x <112379929+Ghostx7x@users.noreply.github.com>
2024-02-26 23:22:31 +0530

    Add files via upload

---
+++ MainClass1.java
@@ -0,0 +1,10 @@
+import mypack.MyClass;
+
+public class MainClass1
+{
+    public static void main(String[] args) 
+    {
+        MyClass myObject = new MyClass();
+        myObject.displayMessage();    
+    }    
+}
